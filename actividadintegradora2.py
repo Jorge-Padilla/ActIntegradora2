09a9f6a7297582115da8c1b111e31d72abd003be
JorgeZavalza10 <85328347+JorgeZavalza10@users.noreply.github.com>
2021-09-15 16:13:10 -0500

    Update actividadintegradora2.py

--- actividadintegradora2.py
+++ actividadintegradora2.py
@@ -796,3 +796,87 @@ def sentiment(text):
     feats = find_features(text)
 
     return voted_classifier.classify(feats)
+
+# Tutorial 20
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import sentiment_mod as s
+
+
+
+#consumer key, consumer secret, access token, access secret.
+ckey="asdfsafsafsaf"
+csecret="asdfasdfsadfsa"
+atoken="asdfsadfsafsaf-asdfsaf"
+asecret="asdfsadfsadfsadfsadfsad"
+
+from twitterapistuff import *
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        try:
+            all_data = json.loads(data)
+
+            tweet = all_data["text"]
+            sentiment_value, confidence = s.sentiment(tweet)
+            print(tweet, sentiment_value, confidence)
+
+            if confidence*100 >= 80:
+                output = open("twitter-out.txt","a")
+                output.write(sentiment_value)
+                output.write('\n')
+                output.close()
+
+            return True
+        except:
+            return True
+
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["happy"])
+
+
+# Tutorial 21
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+import time
+
+style.use("ggplot")
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+    pullData = open("twitter-out.txt","r").read()
+    lines = pullData.split('\n')
+
+    xar = []
+    yar = []
+
+    x = 0
+    y = 0
+
+    for l in lines[-200:]:
+        x += 1
+        if "pos" in l:
+            y += 1
+        elif "neg" in l:
+            y -= 1
+
+        xar.append(x)
+        yar.append(y)
+        
+    ax1.clear()
+    ax1.plot(xar,yar)
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
+
